df5637aa580cf1798d2dc2865e45cb7f020ec25f
1stthomas <thomas.pluess@mesch.ch>
2018-02-10 19:38:41 +0100

    further fibonacci functions added
    implementation of the time measurement
    descriptions added

--- fibonacci.py
+++ fibonacci.py
@@ -2,20 +2,92 @@
 """
 Created on Fri Feb  9 19:03:59 2018
 
-@author: zoom4u
+@author: 1stthomas
 """
-
-index = 0
+import time
 
 
 def fib(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        value = 0
+        i0 = 0
+        i1 = 1
+        for n in range(1, n):
+            value = i0 + i1
+            i0 = i1
+            i1 = value
+        return value
+
+
+def fib1(n):
     """
-    Calculates the Fibonacci number of the specified step
+    Calculates the Fibonacci number of the specified step 'n'.
 
     Parameters
     ----------
     n : number
-      The step to be calculated
+      The step of the Fibonacci sequence to be calculated.
+
+    Returns
+    -------
+    res : number
+      The value of the n th step of the Fibonacci.
+
+    Example
+    -------
+    >>> fib1(10)
+    55
+    """
+
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib1(n - 1) + fib1(n - 2)
+
+
+def fibCallsCount(n):
+    """
+    Calculates the count of function calls to get the Fibonacci number
+    of the specified step 'n'.
+
+    Parameters
+    ----------
+    n : number
+      The step of the Fibonacci sequence to be calculated.
+
+    Returns
+    -------
+    res : number
+      The total count of calls of this function.
+
+    Example
+    -------
+    >>> fibCallsCount(10)
+    177
+    """
+
+    i = 1
+    if n >= 2:
+        i1 = fibCallsCount(n - 1)
+        i2 = fibCallsCount(n - 2)
+        i += i1 + i2
+
+    return i
+
+
+def fib2(n):
+    """
+    Calculates the Fibonacci number of the specified step 'n'.
+    This method also counts the calls of this method. You can get the count
+    by reseting the fib2.count variable to zero, and get its value after
+    the execution of this function (see the example).
+
+    Parameters
+    ----------
+    n : number
+      The step of the Fibonacci sequence to be calculated
 
     Returns
     -------
@@ -24,34 +96,71 @@ def fib(n):
 
     Example
     -------
-    >>> fib(10)
+    >>> fib2.count = 0
+    >>> fib2(10)
     55
+    >>> fib2.count
+    177
     """
-    global index
-    index = index + 1
-    
+
+    fib2.count += 1
     if n == 0 or n == 1:
         return n
     else:
-        return fib(n - 1) + fib(n - 2)
+        return fib2(n - 1) + fib2(n - 2)
 
 
-def fibCallsCount(n):
-    print(fibCallsCount.count)
-    fibCallsCount.count = fibCallsCount.count + 1
-    if n == 0 or n == 1:
-        return n
+def printOutput(f_name, value_type, value):
+    """
+    Outputs a formated string of the submitted parameters.
+
+    Parameters
+    ----------
+    f_name : string
+      The function name.
+    value_type : string
+      The type of the value.
+    value : mixed
+      The value to be printed.
+
+    Example
+    -------
+    >>> printOutput("fib2(" + str(10) + ")", "value", fib2(10))
+    Funktionsaufruf fib2(10) - value: 55
+    """
+
+    output = "Funktionsaufruf " + str(f_name) + " - "
+    if value_type == "value":
+        output += "value: "
     else:
-        return fib(n - 1) + fib(n - 2)
+        output += "Anzahl Funktionsaufrufe: "
+
+    output += str(value)
+
+    print(output)
 
 
-print(fib(10))
+"""
+@var step : the step of the fibunacci sequence to be calculated.
+"""
+step = 40
+
 print("---------------------------")
-print("index: " + str(index))
+print("step: " + str(step))
+t_start = time.process_time()
+printOutput("fib(" + str(step) + ")", "value", fib(step))
+t_end = time.process_time()
+print("Zeitdifferenz: " + str(t_end - t_start))
 print("---------------------------")
-
-fibCallsCount.count = 0
-
-print(fibCallsCount(10))
+print("step: " + str(step))
+t_start = time.process_time()
+printOutput("fib1(" + str(step) + ")", "value", fib1(step))
+t_end = time.process_time()
+print("Zeitdifferenz: " + str(t_end - t_start))
 print("---------------------------")
-print("count: " + str(fibCallsCount.count))
+fib2.count = 0
+printOutput("fib2(" + str(step) + ")", "value", fib2(step))
+printOutput("fib2(" + str(step) + ")", "count", fib2.count)
+print("---------------------------")
+count = fibCallsCount(step)
+printOutput("fibCallsCount(" + str(step) + ", 0)", "count", count)
